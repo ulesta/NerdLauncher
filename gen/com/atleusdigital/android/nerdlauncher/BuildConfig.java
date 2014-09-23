e0e6cdce8db9cbd93d6c4e31d8907e37906a44b2
Justin Cuaresma <justincuaresma@gmail.com>
2014-09-23 15:04:39 -0400

    [Ch23 - started] More About Intents and Tasks
    Created new project - NerdLauncher
    
    Added:
    + Copied over SingleFragmentActivity class from CriminalIntent
    + Copied over activity_fragment.xml layout from CriminalIntent

---
+++ gen/com/atleusdigital/android/nerdlauncher/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.atleusdigital.android.nerdlauncher;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/atleusdigital/android/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,16 @@
+package com.atleusdigital.android.nerdlauncher;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
